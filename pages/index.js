0e80a5416a13266e92fbdf256bc605dc46554ecc
Ascen Salmerón <ascensalmanez@gmail.com>
2020-08-10 20:03:03 +0200

    another change

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">{data.name}</a>
         </h1>
 
         <p className={styles.description}>
