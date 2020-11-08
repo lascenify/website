120b6a3878a3cc200b639214641acc51e2261838
Ascen Salmerón <ascensalmanez@gmail.com>
2020-11-08 19:19:52 +0100

    feat(index): convert file to typescript and replace components imports

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import Container from '@components/container';
+import Layout from '@components/layout';
+export default function Home() {
+  return (
+    <>
+      <Layout preview="">
+        <Head>
+          <title>Ascen Salmerón</title>
+        </Head>
+        <Container>Hello, I'm Ascen, a software developer</Container>
+      </Layout>
+    </>
+  );
+}
